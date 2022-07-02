b9d38f1855818653bbb42e89e9362262259ce759
Nicolas <chinaglianr@gmail.com>
2022-07-01 21:27:43 -0300

    project

---
+++ NICOLAS ROBERTO CHINAGLIA/scripts/script.js
@@ -0,0 +1,16 @@
+
+
+function clicar() {
+  var lamp = document.getElementById('biglamp')
+  lamp.classList.toggle("animalampmaior");
+}
+
+function clicklamp() {
+  var lump = document.getElementById('minilamp')
+  lump.classList.toggle("animalampmenor");
+}
+
+function risada() {
+  var som = document.getElementById('tocamulher')
+  som.play();
+}
